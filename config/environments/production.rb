10ea38b503f60e7ca03841522bbc47f8fa7b429b
Jack Kiarie <jacknkiarie@gmail.com>
2020-06-08 16:55:32 +0300

    added action cable ready to deploy

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,4 +83,8 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_cable.url = 'wss://calm-falls-14818.herokuapp.com/cable'
+  config.action_cable.allowed_request_origins = [
+      'https://calm-falls-14818.herokuapp.com/' ]
 end
